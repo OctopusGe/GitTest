27da076f742e5906d1b1a7807706c8489d126d9f
黄石章 <46342150+OctopusGe@users.noreply.github.com>
2019-07-15 13:50:00 +0800

    IDEA整合GitHub测试

---
+++ src/FirstCode.java
@@ -0,0 +1,8 @@
+/**
+ * @author stone(huangshizhang) at 2019-07-15 13:43
+ */
+public class FirstCode {
+    public static void main(String[] args) {
+        System.out.println("hello,word");
+    }
+}
